fdc12b707146bfe5f01605bcb5916d191ee54b67
craines <marquinhossoares1093@hotmail.com>
2022-05-16 14:52:21 -0300

    correção

--- drivers/superchat/api/index.js
+++ drivers/superchat/api/index.js
@@ -12,26 +12,12 @@ const headers = {
     "token": process.env.TOKEN_SYSTEM
 }
 
-async function sendText (job) {
+async function send (job) {
     try {
-        const response = await axios.post('http://localhost:3001/whatsapp/send/text', job.data.data, {
+        const response = await axios.post(`http://localhost:3001/whatsapp/${job.name}`, job.data.data, {
             headers: headers
         })
         console.log('STATUS', response.data.status)
-        if (response.data.status !== 200) {
-            console.log('Error: ', response.data)
-            throw "Error"
-        }
-    } catch (e) {
-        throw e
-    }
-}
-
-async function sendButtons (job) {
-    try {
-        const response = await axios.post('http://localhost:3001/whatsapp/send/buttons', job.data.data, {
-            headers: headers
-        })
         if (response.data.status !== 200) {
             console.log('Error: ', response.data)
             throw "Error"
@@ -51,21 +37,16 @@ SUPERCHAT_LICENSE: ${process.env.SUPERCHAT_LICENSE}
 
 
 const worker = new Worker(process.env.BOT_NAME, async job => {
-    await sleep(job.data.delay)
-    console.log('Processando job:', job.name)
-    const methods = {
-        'send_text': sendText,
-        'send_buttons': sendButtons
-    }
+        await sleep(job.data.delay)
+        console.log('Processando job:', job.name)
+        await send(job)
 
-    await methods[job.name](job)
-
-}, {
-    connection: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
+    }, {
+        connection: {
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT
+        }
     }
-}
 );
 
 worker.on('completed', (job) => {
